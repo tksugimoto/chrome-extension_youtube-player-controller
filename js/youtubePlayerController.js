ef4460b3733b2b79cebb92cf0c59833a3fb5cb08
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-12-18 22:49:06 +0900

    [Add] ホイールで音量変更・シーク機能

---
+++ js/youtubePlayerController.js
@@ -0,0 +1,53 @@
+execScript(() => {
+	"use strict";
+
+	document.body.addEventListener("wheel", evt => {
+		if (evt.deltaY === 0) return;
+
+		const volumePanel = evt.target.closest("#movie_player .ytp-volume-panel");
+		if (volumePanel) {
+			// YouTube公式のボリューム変更バー
+			return;
+		}
+
+		const videoArea = evt.target.closest("#movie_player .html5-video-container video");
+		if (videoArea) {
+			// 下方向回転でボリュームを小さくする
+			Video.changeVolume(-5 * Math.sign(evt.deltaY));
+			evt.preventDefault();
+			return;
+		}
+		const bottomBar = evt.target.closest("#movie_player .ytp-chrome-bottom");
+		if (bottomBar) {
+			// 下方向回転で先送り
+			Video.seekBy(5 * Math.sign(evt.deltaY));
+			evt.preventDefault();
+			return;
+		}
+	});
+
+	function getPlayer() {
+		return document.getElementById("movie_player");
+	}
+	const Video = {
+		changeVolume: diff => {
+			const player = getPlayer();
+			let volume = player.getVolume() + diff;
+			player.setVolume(volume);
+			if (player.isMuted()) player.unMute();
+		},
+
+		seekBy: diff => {
+			getPlayer().seekBy(diff);
+		}
+	};
+});
+
+function execScript(fn){
+	const script_text = "(" + fn.toString() + ")()";
+	const script = document.createElement("script");
+	script.appendChild(document.createTextNode(script_text));
+	const parent = document.head;
+	parent.appendChild(script);
+	parent.removeChild(script);
+}
